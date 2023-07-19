1c32bd37847f3638a5f36571539b30b08fc68518
Mclina <mariacelinoh@gmail.com>
2023-07-19 14:54:59 -0700

    101-natural.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,35 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
- * main - A program that prints combination of all two digit numbers.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+*print_times_table - prints the  times table with
+*parameter
+*@n: parameter
+*Return: returns nothing
+*/
+void print_times_table(int n)
 {
+int digit, mult, result;
+if (n <= 15 && n >= 0)
+{
+for (digit = 0; digit <= n; digit++)
+{
+_putchar('0');
 
-	int num = 0;
-	int tens;
-	int ones;
+for (mult = 1; mult <= n; mult++)
+{
+_putchar(',');
+_putchar(' ');
+result = digit * mult;
+if (result <= 99)
+_putchar(' ');
 
-	while (num <= 99)
-	{
-		tens = num % 10;
-		ones = num / 10;
-
-		if (ones < tens)
-		{
-			putchar(ones + '0');
-			putchar(tens + '0');
-			if (num < 89)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-		num++;
-	}
-	putchar('\n');
-
-	return (0);
+if (result <= 9)
+_putchar(' ');
+if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar((result / 10) % 10 + '0');
+}
+else if (result <= 99 && result >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n');
+}
+}
 }
