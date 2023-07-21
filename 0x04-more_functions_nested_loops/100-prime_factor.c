221313b092412277e37f9c80a7884358925cf690
Mclina <mariacelinoh@gmail.com>
2023-07-21 16:00:03 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,41 +1,31 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
-*print_times_table - prints the  times table with
-*parameter
-*@n: parameter
-*Return: returns nothing
+*main- prints the largest prime factor
+*of a number
+*
+*Return: returns 0
 */
-void print_times_table(int n)
-{
-int digit, mult, result;
-if (n <= 15 && n >= 0)
-{
-for (digit = 0; digit <= n; digit++)
-{
-_putchar('0');
 
-for (mult = 1; mult <= n; mult++)
+int main(void)
 {
-_putchar(',');
-_putchar(' ');
-result = digit * mult;
-if (result <= 99)
-_putchar(' ');
+long number = 612852475143;
+int inc;
+while (inc++ < number / 2)
+{
+if (number % inc == 0)
+{
+number /= 2;
+continue;
+}
 
-if (result <= 9)
-_putchar(' ');
-if (result >= 100)
+for (inc = 3; inc < number / 2; inc += 2)
 {
-_putchar((result / 100) + '0');
-_putchar((result / 10) % 10 + '0');
-}
-else if (result <= 99 && result >= 10)
-{
-_putchar((result / 10) + '0');
-}
-_putchar((result % 10) + '0');
-}
-_putchar('\n');
+if (number % inc == 0)
+number /= inc;
+
 }
 }
+printf("%ld\n", number);
+return (0);
 }
