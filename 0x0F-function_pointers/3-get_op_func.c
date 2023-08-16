5dd560d1e3f577c284b912192c3bfe692af69067
Mclina <mariacelinoh@gmail.com>
2023-08-16 16:56:47 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,27 +1,65 @@
 #include "3-calc.h"
+#include <stdlib.h>
+
 /**
-* get_op_func - Selects the function to execute based on the operator.
-* @s: The operator used.
-* Return: Pointer to the appropriate function.
-*/
+*get_op_func - selects the correct function to p#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * *get_op_func - selects the correct function to perform
+ * *the operation asked by the us#include "3-calc.h"
+ * #include <stdlib.h>
+ *
+ */**
+*get_op_func - selects the correct function to perform
+*the operation ask#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * *get_op_func - selects the correct function to perform
+ * *the operation asked by the us#include "3-calc.h"
+ * #include <stdlib.h>
+ *
+ */**
+*get_op_func - selects the correct function to perform
+*the operation ask#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * *get_op_func - selects the correct function to perform
+ * *the operation asked by the us#include "3-calc.h"
+ * #include <stdlib.h>
+ *
+ */**
+*get_op_func - selects the correct function to perform
+*the operation ask#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * *get_op_func - selects the correct function to perform
+ * *the operation asked by the user. You’re not allowed
+ * *to declare any other function.
+ * *@s: operator passed as argument
+ * *
+ * *Return: A pointer to the function corresponding to
+ * *the operator given in parameter
+ * */
 int (*get_op_func(char *s))(int, int)
 {
+
 	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL}};
+		{NULL, NULL}
+	};
+	int i = 0;
 
-	int i;
-
-	i = 0;
-	while (ops[i].op)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (NULL);
+
+	return (ops[i].f);
+
 }
